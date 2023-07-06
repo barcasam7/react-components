4321c098369d323c5c0cd86c7417205a9babdb82
Sam Ward <sam@wordnerds.ai>
2023-07-06 20:37:27 +0100

    add search functionality to github api

--- src/components/GithubProfile.tsx
+++ src/components/GithubProfile.tsx
@@ -1,50 +1,77 @@
+import axios from "axios";
 import { useState } from "react";
 
+type profile = {
+  id: number;
+  name: string;
+  followers: number;
+  following: number;
+  avatar_url: string;
+  bio: string;
+  public_repos: number;
+};
+
 const GithubProfile = () => {
+  const [user, setUser] = useState<profile | null>(null);
+  const [search, setSearch] = useState<string>("");
+
+  const searchProfile = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    setSearch(target.value);
+  };
+
+  const getUsers = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${search}`)
+      .then(data => setUser(data.data))
+      .catch(error => setUser(null));
+  };
   return (
     <>
-      <form className="user-form" id="form">
-        <input type="text" id="search" placeholder="Search a Github User" />
+      <form className="user-form" id="form" onSubmit={e => getUsers(e)}>
+        <input
+          type="text"
+          id="search"
+          placeholder="Search a Github User"
+          onChange={e => searchProfile(e)}
+        />
       </form>
       <main id="main">
-        <div className="card">
-          <div>
-            <img
-              src="https://randomuser.me/api/portraits/men/30.jpg"
-              alt=""
-              className="avatar"
-            />
-          </div>
-          <div className="user-info">
-            <h2>John Doe</h2>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque,
-              ducimus?
-            </p>
-            <ul>
-              <li>
-                300 <strong>Followers</strong>
-              </li>
-              <li>
-                300 <strong>Following</strong>
-              </li>
-              <li>
-                3 <strong>Repos</strong>
-              </li>
-            </ul>
-            <div id="repos">
-              <a href="" className="repo">
-                Repo
-              </a>
-              <a href="" className="repo">
-                Repo
-              </a>
-              <a href="" className="repo">
-                Repo
-              </a>
+        {user !== null && (
+          <div className="card">
+            <div>
+              <img src={user.avatar_url} alt="" className="avatar" />
+            </div>
+            <div className="user-info">
+              <h2>{user.name}</h2>
+              <p>{user.bio}</p>
+              <ul>
+                <li>
+                  {user.followers} <strong>Followers</strong>
+                </li>
+                <li>
+                  {user.following} <strong>Following</strong>
+                </li>
+                <li>
+                  {user.public_repos} <strong>Repos</strong>
+                </li>
+              </ul>
+              <div id="repos">
+                <a href="" className="repo">
+                  Repo
+                </a>
+                <a href="" className="repo">
+                  Repo
+                </a>
+                <a href="" className="repo">
+                  Repo
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        )}
+        {user === null && <p>No user found for that username</p>}
       </main>
     </>
   );
