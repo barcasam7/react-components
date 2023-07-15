b167a8a5964288659f170c577d10441f1221d821
Sam Ward <sam@wordnerds.ai>
2023-07-15 18:30:28 +0100

    add skeleton loading

--- src/components/GithubProfile.tsx
+++ src/components/GithubProfile.tsx
@@ -1,91 +1,99 @@
 import axios from "axios";
 import { useState } from "react";
+import Skeleton from "./Skeleton";
 
 type profile = {
-  id: number;
-  name: string;
-  followers: number;
-  following: number;
-  avatar_url: string;
-  bio: string;
-  public_repos: number;
+   id: number;
+   name: string;
+   followers: number;
+   following: number;
+   avatar_url: string;
+   bio: string;
+   public_repos: number;
 };
 
 type repo = {
-  name: string;
-  html_url: string;
+   name: string;
+   html_url: string;
 };
 
 const GithubProfile = () => {
-  const [user, setUser] = useState<profile | null>(null);
-  const [search, setSearch] = useState<string>("");
-  const [repos, setRepos] = useState<repo[]>([]);
-  const [userNotFound, setUserNotFound] = useState<boolean>(false);
+   const [user, setUser] = useState<profile | null>(null);
+   const [search, setSearch] = useState<string>("");
+   const [repos, setRepos] = useState<repo[]>([]);
+   const [userNotFound, setUserNotFound] = useState<boolean>(false);
+   const [isLoading, setLoading] = useState<boolean>(false);
 
-  const searchProfile = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const target = event.target as HTMLInputElement;
-    setSearch(target.value);
-  };
+   const searchProfile = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const target = event.target as HTMLInputElement;
+      setSearch(target.value);
+   };
 
-  const getUsers = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    axios
-      .get(`https://api.github.com/users/${search}`)
-      .then(data => {
-        setUser(data.data);
-        getRepos();
-        setUserNotFound(false);
-      })
-      .catch(() => setUserNotFound(true));
-  };
+   const getUsers = (event: React.FormEvent<HTMLFormElement>): void => {
+      setLoading(true);
+      event.preventDefault();
+      axios
+         .get(`https://api.github.com/users/${search}`)
+         .then((data) => {
+            setUser(data.data);
+            getRepos();
+            setUserNotFound(false);
+            setLoading(false);
+         })
+         .catch(() => {
+            setUserNotFound(true);
+            setLoading(false);
+         });
+   };
 
-  const getRepos = (): void => {
-    axios
-      .get(`https://api.github.com/users/${search}/repos`)
-      .then(data => setRepos(data.data.slice(0, 10)))
-      .catch(() => setRepos([]));
-  };
+   const getRepos = (): void => {
+      axios
+         .get(`https://api.github.com/users/${search}/repos`)
+         .then((data) => setRepos(data.data.slice(0, 10)))
+         .catch(() => setRepos([]));
+   };
 
-  return (
-    <>
-      <form className="user-form" id="form" onSubmit={e => getUsers(e)}>
-        <input type="text" id="search" placeholder="Search a Github User" onChange={e => searchProfile(e)} />
-      </form>
-      <main id="main">
-        {user !== null && (
-          <div className="card">
-            <div>
-              <img src={user.avatar_url} alt="" className="avatar" />
-            </div>
-            <div className="user-info">
-              <h2>{user.name}</h2>
-              <p>{user.bio}</p>
-              <ul>
-                <li>
-                  {user.followers} <strong>Followers</strong>
-                </li>
-                <li>
-                  {user.following} <strong>Following</strong>
-                </li>
-                <li>
-                  {user.public_repos} <strong>Repos</strong>
-                </li>
-              </ul>
-              {repos.length > 0 && (
-                <div id="repos">
-                  {repos.map(repo => (
-                    <a target="_blank" href={repo.html_url} className="repo">
-                      {repo.name}
-                    </a>
-                  ))}
-                </div>
-              )}
-            </div>
-          </div>
-        )}
-        {userNotFound && <p>No user found for that username</p>}
-      </main>
-    </>
-  );
+   return (
+      <>
+         <form className="user-form" id="form" onSubmit={(e) => getUsers(e)}>
+            <input type="text" id="search" placeholder="Search a Github User" onChange={(e) => searchProfile(e)} />
+         </form>
+         <main id="main">
+            {isLoading && <Skeleton />}
+            {user !== null && !userNotFound && !isLoading && (
+               <div className="card">
+                  <div>
+                     <img src={user.avatar_url} alt="" className="avatar" />
+                  </div>
+                  <div className="user-info">
+                     <h2>{user.name}</h2>
+                     <p>{user.bio}</p>
+                     <ul>
+                        <li>
+                           {user.followers} <strong>Followers</strong>
+                        </li>
+                        <li>
+                           {user.following} <strong>Following</strong>
+                        </li>
+                        <li>
+                           {user.public_repos} <strong>Repos</strong>
+                        </li>
+                     </ul>
+                     {repos.length > 0 && (
+                        <div id="repos">
+                           {repos.map((repo) => (
+                              <a target="_blank" href={repo.html_url} className="repo">
+                                 {repo.name}
+                              </a>
+                           ))}
+                        </div>
+                     )}
+                  </div>
+               </div>
+            )}
+            {userNotFound && !isLoading && <p>No user found for that username</p>}
+         </main>
+      </>
+   );
 };
 export default GithubProfile;
---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,16 @@
+const Skeleton = () => {
+   return (
+      <div className="card skeleton">
+         <div>
+            <img alt="" className="avatar" />
+         </div>
+         <div className="user-info">
+            <h2></h2>
+            <p></p>
+            <p></p>
+            <p></p>
+         </div>
+      </div>
+   );
+};
+export default Skeleton;
